019e27103783f751e6f025a2d4ac1bd286fa54ed
santyelegy <santyelegy@gmail.com>
2023-01-14 17:28:08 -0500

    add simple search and filter

--- frontend/src/Pages/CourseList.js
+++ frontend/src/Pages/CourseList.js
@@ -1,26 +1,72 @@
 import dept_course from '../sample_data/CourseList.json'
 import { useParams, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Form from 'react-bootstrap/Form';
 
 function CourseList() {
-    const { department } = useParams();
-    const course = dept_course[0].cources.map((cources, index) => {
-        return (
-            <div>
-                <Link to={"/courselist/course/".concat(index)}>{dept_course[0].cources[0].code + " " + dept_course[0].cources[0].name}</Link></div>
-        );
+    const { department_id } = useParams();
+    const [allcourses, setAllCourses] = useState([]);
+    const [search, setSearch] = useState("");
+    const [department, setDepartment] = useState([true, true, true]);
+    useEffect(() => {
+        let subarray = []
+        for (let i = 0; i < dept_course.length; i++) {
+            if(department[i]){
+                subarray.push(...dept_course[i]["cources"]);
+            }
+        }
+        setAllCourses(subarray);
+    }, [JSON.stringify(department)])
+    const courses = allcourses.map((course, index) => {
+        let show = true;
+        if (!course.code.toLowerCase().startsWith(search.toLowerCase()) &&
+            !course.name.toLowerCase().startsWith(search.toLowerCase())) {
+            show = false;
+        }
+
+        return show ? (<div index={index}>
+            <Link to={"/courselist/course/".concat(course.code)}>{course.code.concat(" ", course.name)}</Link>
+            <br />
+        </div>) : <div index={index}></div>;
     })
+    function handleBoxChange(event,index){
+        setDepartment((prev) => prev.map((el, i) => (i !== index ? el : event.target.checked)));
+    }
     return (
         <>
             <div>
-                <div>
-                    {course}</div>
-                <div>
-                    {dept_course[0].cources[0].prof}</div>
-
-                <div>
-                    {course}</div>
-                <div>
-                    {dept_course[0].cources[1].prof}</div>
+                <Form>
+                    <Form.Label>
+                        Search
+                    </Form.Label>
+                    <Form.Control type="text" placeholder="Enter Course Name/Code"
+                        onChange={(e) => setSearch(e.target.value)} />
+                    <Form.Label>
+                        Department Filter:
+                    </Form.Label>
+                    <Form.Check
+                        inline
+                        label="CS"
+                        type='checkbox'
+                        defaultChecked={department[0]}
+                        onChange={(e)=>handleBoxChange(e,0)}
+                    />
+                    <Form.Check
+                        inline
+                        label="ECE"
+                        type='checkbox'
+                        defaultChecked={department[1]}
+                        onChange={(e)=>handleBoxChange(e,1)}
+                    />
+                    <Form.Check
+                        inline
+                        label="DS"
+                        type='checkbox'
+                        defaultChecked={department[2]}
+                        onChange={(e)=>handleBoxChange(e,2)}
+                    />
+                </Form>
+                {courses}
             </div>
         </>
     );
--- frontend/src/Pages/ProfessorList.js
+++ frontend/src/Pages/ProfessorList.js
@@ -1,29 +1,40 @@
 import professorlist from "../sample_data/ProfessorList.json"
 import Accordion from 'react-bootstrap/Accordion';
 import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
 function ProfessorList() {
-    function subaccordion(professor,index){
-        const coursegroup=professor.course.map((course,i)=>{
-            console.log(course);
-            return (<><Link to={"/courselist/course/".concat(course)} index={i}>{course}</Link><br/></>);    
-        });
-        return (
-            <Accordion.Item eventKey={index}>
-          <Accordion.Header>{<Link to={"/professor/".concat(professor.name)}>{professor.name.concat('# score:',professor.score)}</Link>}</Accordion.Header>
-          <Accordion.Body>
-            {coursegroup}
-          </Accordion.Body>
-        </Accordion.Item>
-        );
-        
-    }
-    const all_professor=professorlist.map((professor,index)=>{
-        return (subaccordion(professor,index));
-    })
+  const [search,setSearch]=useState("");
+  function subaccordion(professor, index) {
+    const coursegroup = professor.course.map((course, i) => {
+      return (<><Link to={"/courselist/course/".concat(course)} index={i}>{course}</Link><br /></>);
+    });
     return (
-        <Accordion>
+      <Accordion.Item eventKey={index}>
+        <Accordion.Header>{<Link to={"/professor/".concat(professor.name)}>{professor.name.concat('# score:', professor.score)}</Link>}</Accordion.Header>
+        <Accordion.Body>
+          Courses:<br/>
+          {coursegroup}
+        </Accordion.Body>
+      </Accordion.Item>
+    );
+  }
+  const all_professor = professorlist.map((professor, index) => {
+    return professor.name.startsWith(search)?(subaccordion(professor, index)):<></>;
+  })
+  return (
+    <>
+      <Form>
+        <Form.Label>
+          Search
+        </Form.Label>
+        <Form.Control type="text" placeholder="Enter Professor Name"
+          onChange={(e) => setSearch(e.target.value)} />
+      </Form>
+      <Accordion>
         {all_professor}
       </Accordion>
-    );
+    </>
+  );
 }
 export default ProfessorList;
