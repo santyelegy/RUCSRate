18bcd6830afff8fca049f0c0c46be68ea0fee170
XUQIPAN <1668112439@qq.com>
2023-01-20 17:45:50 -0500

    optimize imports

--- backend/api/src/main/java/com/rucsrate/api/repository/ReviewRepository.java
+++ backend/api/src/main/java/com/rucsrate/api/repository/ReviewRepository.java
@@ -1,11 +1,8 @@
 package com.rucsrate.api.repository;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.rucsrate.api.model.Course;
 import com.rucsrate.api.model.Review;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
